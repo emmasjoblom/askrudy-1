71e5ab9651990e2432965a25a85a171e85514243
Pierre Milliotte <pmilliotte@gmail.com>
2018-08-08 18:41:00 +0800

    setting customer devise and basic navbar for homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_action :authenticate_customer!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name,
+      :last_name,
+      :phone_number,
+      :address,
+      :languages,
+      :birthdate
+    ])
+  end
 end
